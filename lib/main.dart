eab434a2ac9034437ac7a9838cd2e4f807cf4789
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-07 12:25:57 -0500

    Added some extra TODOs and completed the solution for step 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_celebrities_2/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,11 +22,14 @@ void main() {
 // TODO (5): Type "stl" and select "New stateless widget"
 // We will now create a stateless widget to represent our HomeScreen.
 // TODO (6): Name the new widget as "HomeScreen"
+// TODO (7): Click on any of the errors
+// TODO (8): Press Alt+Enter (Ctrl+. on VS Code)
+// TODO (9): Import the material package.
 
-// TODO (7): Now go to main.dart
-// TODO (8): The "home" attribute of the MaterialApp (MyApp) should now point to the new widget.
+// TODO (10): Now go to main.dart
+// TODO (11): The "home" attribute of the MaterialApp (MyApp) should now point to the new widget.
 
-// TODO (9): Run the app. You should see a blank black screen.
+// TODO (12): Run the app. You should see a blank black screen.
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -49,93 +53,9 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
 // TODO (2): Delete "MyHomePage"
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
