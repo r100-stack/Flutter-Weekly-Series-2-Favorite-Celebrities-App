97331a610a60dcec90f32fa97d963d960ca4e527
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-07 13:08:35 -0500

    Progress on TODOs for step 3

--- lib/widgets/celeb_card.dart
+++ lib/widgets/celeb_card.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+// We still can't see this CelebCard() even though we placed it in the Column on the HomeScreen.
+// This is because we haven't provided it a child, height, width, or other similar constraints.
+// In cases like these, the Container() shrinks to the smallest it can go to.
+// Hence, to see it, let's give it a height, width, and color.
+
+// TODO (1): Add a height of 200.0
+
+// TODO (11): Run the app. You should see a blank white screen.
+
 class CelebCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
