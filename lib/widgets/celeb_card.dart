64b5ae585a203638aa49335eabaef7039b5ca521
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-08 15:43:38 -0500

    Removed old TODOs

--- lib/widgets/celeb_card.dart
+++ lib/widgets/celeb_card.dart
@@ -1,25 +1,5 @@
 import 'package:flutter/material.dart';
 
-// Observe how our CelebCard seems to stick to the corner a little too much.
-// Also, there is no spacing between our name and tagline Text() widgets.
-// To solve this problem, let's add some padding to the container.
-// Padding and Margins are represented using the EdgeInsets.<...>().
-// Here <...> could be symmetric, all, only, etc, depending on our needs.
-// TODO (1): To the padding property to the Container
-// In our case, let's use the EdgeInsets.symmetric version of EdgeInsets
-// TODO (2): Assign the padding property to EdgeInsets.symmetric().
-// TODO (3): Give a horizontal padding of 20.0, and a vertical padding of 10.0
-// Let's also add a margin to increase spacing
-// TODO (4): Assign the margin property to EdgeInsets.symmetric().
-// TODO (5): Give a horizontal margin of 20.0, and a vertical margin of 20.0
-// Now to space the Text() widget from each other and also from the image, let's use a SizedBox()
-// A SizedBox is a shrunk box by default. We can give it a height or width if needed.
-// Because of this property of not having any size by itself, we common use it for spacing.
-// TODO (6): Add a SizedBox between the Image() and the name Text().
-// TODO (7): Give it a height of 20.0
-// TODO (8): Add a SizedBox between the name Text() and the tagline Text().
-// TODO (9): Give it a height of 5.0
-
 class CelebCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
