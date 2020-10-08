133a4c5c064f382c15522fc54369f1a8e2ca2bd7
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-08 15:30:35 -0500

    Rearranged TODOs in home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,13 @@
 import 'package:favorite_celebrities_2/widgets/celeb_card.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  // TODO (12): Click on Flutter Inspector -> Show Debug Paint
-  // Notice how the column is only as wide as the Column needs to be.
-  // To change this, add the CrossAxisAlignment.stretch property for the Column()
-  // Then the Column will stretch regardless of it's contents
-  // TODO (13): For "crossAxisAlignment" of Column, assign CrossAxisAlignment.stretch
+// TODO (12): Click on Flutter Inspector -> Show Debug Paint
+// Notice how the column is only as wide as the Column needs to be.
+// To change this, add the CrossAxisAlignment.stretch property for the Column()
+// Then the Column will stretch regardless of it's contents
+// TODO (13): For "crossAxisAlignment" of Column, assign CrossAxisAlignment.stretch
 
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
