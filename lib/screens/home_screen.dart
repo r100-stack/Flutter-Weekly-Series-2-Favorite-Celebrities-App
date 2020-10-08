0bd6e343c37a5cfe4897efee2e2f24193d55fbe9
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-08 15:27:02 -0500

    Completed solution for step 3

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:favorite_celebrities_2/widgets/celeb_card.dart';
 import 'package:flutter/material.dart';
 
+// TODO (12): Click on Flutter Inspector -> Show Debug Paint
+// Notice how the column is only as wide as the Column needs to be.
+// To change this, add the CrossAxisAlignment.stretch property for the Column()
+// Then the Column will stretch regardless of it's contents
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,6 +15,7 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.pink,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [CelebCard()],
       ),
     );
--- lib/widgets/celeb_card.dart
+++ lib/widgets/celeb_card.dart
@@ -3,18 +3,64 @@ import 'package:flutter/material.dart';
 // We still can't see this CelebCard() even though we placed it in the Column on the HomeScreen.
 // This is because we haven't provided it a child, height, width, or other similar constraints.
 // In cases like these, the Container() shrinks to the smallest it can go to.
-// Hence, to see it, let's give it a height, width, and color.
+// Hence, to see it, let's start adding children to the container.
 
-// TODO (1): Add a height of 200.0
+// TODO (1): Add a color of Colors.pink[100] to the Container (Can change the color to match your liking)
 
-// TODO (11): Run the app. You should see a blank white screen.
+// We should now see a pink container on the screen.
+// Let's now fill this container.
+
+// Notice how our Container has a vertical arrangement of widgets.
+// What widget do we use for vertical arrangement of widgets?
+// Yup, we use a Column!
+// TODO (2): Add a Column() to the child property of the Container()
+// Our first child is our celebrity's image.
+// Primarily, there are two ways to include images in our apps.
+// 1. AssetImage () downloaded locally from the beginning.
+// 2. NetworkImage() downloaded from the internet at runtime.
+// For our app, let's use a NetworkImage.
+// TODO (3): Add an Image() widget at the first child of the Column()
+// The Image() widget takes an ImageProvider as its "image" attribute
+// Here is where we can provider our NetworkImage() or AssetImage().
+// TODO (4): Add a NetworkImage to the image attribute
+// Different images can have different dimensions.
+// Having different dimens of each image can make the app look asymmetric.
+// Hence, to make all image be of the same size, let's give a constant height and width.
+// Also, let's give it a "fit" to make it fit the specified height and width.
+// TODO (5): Give the Image() a height and width of 200.0
+// TODO (6): Give the Image() a "fit" of BoxFit.cover
+// BoxFit.cover is similar to CenterCrop in Android
+// TODO (7): Add a Text() widget to the Column() with appropriate style (24.0, w600) and center alignment
+// TODO (8): This should hold the celebrity's name
+// TODO (9): Add a Text() widget to the Column() with appropriate style (16.0, w300) and center alignment
+// TODO (10): This should hold the celebrity's tagline
+
+// TODO (11): Run the app. You should see the newly created CelebCard stuck to the left of the screen.
 
 class CelebCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 250.0,
       color: Colors.pink[100],
+      child: Column(
+        children: [Image(
+          image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Conan_O%27Brien_by_Gage_Skidmore_2.jpg/1200px-Conan_O%27Brien_by_Gage_Skidmore_2.jpg'),
+          height: 200.0,
+          width: 200.0,
+          fit: BoxFit.cover,
+        ),
+          Text(
+            'Conan O\'Brien',
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            '"Longest running late night show host"',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w300),
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     );
   }
 }
