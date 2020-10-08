07a59fbbb52338208b18e3ccc306b1212d292898
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-08 15:28:59 -0500

    Completed TODOs for step3

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,12 @@ import 'package:favorite_celebrities_2/widgets/celeb_card.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  // TODO (12): Click on Flutter Inspector -> Show Debug Paint
+  // Notice how the column is only as wide as the Column needs to be.
+  // To change this, add the CrossAxisAlignment.stretch property for the Column()
+  // Then the Column will stretch regardless of it's contents
+  // TODO (13): For "crossAxisAlignment" of Column, assign CrossAxisAlignment.stretch
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
